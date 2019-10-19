2c240fa4fbf661f364378f93990e3fa1a6bdfefb
Sayantan <sahasayantan6@gmail.com>
2019-10-19 16:27:39 +0530

    Added ExpressJS Server config (#13)
    * Added 5 mock article datas
    
    * updated file
    
    Had a syntax error due to extra comma, which has been removed
    
    * Added Expressjs server configs

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
-// TODO
-// create a nodejs server here
+const express=require('express')
+const app=express()
+const path=require('path')
+const port=process.env.port || 3000
+//const router= () => require('./router/index.js')
+
+app.use('/static',express.static('static'))
+
+app.get('/', (req,res) => res.sendFile(path.join(__dirname+'/index.html')))
+//app.get('*', (req,res) => console.log('Page Not Found'))
+app.get('*', (req,res) => res.sendFile(path.join(__dirname+'/page_not_found.html')))
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
