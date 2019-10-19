be05e2c7111fab9f8d67d5d2ad088c4d28788233
Ashish Patel <ashish.tripleclicks@gmail.com>
2019-10-19 16:47:20 +0530

    Fix: minor fixes

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const express=require('express')
 const app=express()
 const path=require('path')
 const port=process.env.port || 3000
-//const router= () => require('./router/index.js')
 
 app.use('/static',express.static('static'))
 
 app.get('/', (req,res) => res.sendFile(path.join(__dirname+'/index.html')))
-//app.get('*', (req,res) => console.log('Page Not Found'))
+app.get('/articles', (req, res) => res.sendFile(path.join(__dirname+'/mock/article-data.json')))
 app.get('*', (req,res) => res.sendFile(path.join(__dirname+'/page_not_found.html')))
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
